7b71d867b4bad27b62398fa75fa0defafb47585a
Valentin BONSI (perso) <valentin.bonsi2@gmail.Com>
2023-04-13 14:35:22 +0200

    add gitignore

--- IAM_FLAIR_dev.py
+++ IAM_FLAIR_dev.py
@@ -1,18 +1,22 @@
 from flair.data import Corpus
 from flair.datasets import ColumnCorpus
+from flair.embeddings import (FlairEmbeddings, StackedEmbeddings,
+                              TransformerWordEmbeddings, WordEmbeddings, ELMoEmbeddings)
 from flair.models import SequenceTagger
-from flair.embeddings import TransformerWordEmbeddings, WordEmbeddings, FlairEmbeddings, StackedEmbeddings
 from flair.trainers import ModelTrainer
+from torch.optim import AdamW
+from transformers import LlamaTokenizer
 
 data_folder = 'BIO_FLAIR_FORMAT'
 #define the txt columns
 columns = {0 : 'text', 1: 'ner'}
+dataset = 'rwth_18_all'
 corpus : Corpus = ColumnCorpus(
     data_folder, 
     columns, 
-    'iam_train_custom_6_perfect_BIO.txt', 
-    'iam_test_custom_6_perfect_BIO.txt',
-    'iam_valid_custom_6_perfect_BIO.txt'
+    f'iam_train_{dataset}_BIO.txt', 
+    f'iam_test_{dataset}_BIO.txt',
+    f'iam_valid_{dataset}_BIO.txt'
 )
 
 print(len(corpus.train))
@@ -23,19 +27,22 @@ label_type = 'ner'
 label_dict = corpus.make_label_dictionary(label_type=label_type, add_unk=False)
 print(label_dict)
 
-embeddings = TransformerWordEmbeddings(model='bert-base-uncased', layers='-1', subtoken_pooling='first',fine_tune=True,use_context=False)#use_context=True
+embeddings = TransformerWordEmbeddings(model='chavinlo/gpt4-x-alpaca', layers='-1', subtoken_pooling='first',fine_tune=True,use_context=False)#use_context=True
 tagger = SequenceTagger(hidden_size=256, embeddings=embeddings, tag_dictionary=label_dict, tag_type='ner',use_crf=False,use_rnn=False,reproject_embeddings=False)
 trainer = ModelTrainer(tagger,corpus)
-trainer.fine_tune('resources/taggers/iam_ner_bert_custom_6_perfect_nocontext', learning_rate=5.0e-6, mini_batch_size=4, mini_batch_chunk_size=1, embeddings_storage_mode='cpu')
+trainer.fine_tune(f'resources/taggers/iam_ner_alpaca7b_{dataset}', learning_rate=5.0e-6, mini_batch_size=4, mini_batch_chunk_size=1, embeddings_storage_mode='cpu')
 
 # embedding_types = [
 #     WordEmbeddings('glove'),
 #     FlairEmbeddings('news-forward'),
-#     FlairEmbeddings('news-backward')
+#     FlairEmbeddings('news-backward'),
+#     #TransformerWordEmbeddings(model='elmo-medium', layers='-1', subtoken_pooling='first',fine_tune=True,use_context=False)
 # ]
-
+# optim = AdamW
 # embeddings = StackedEmbeddings(embeddings=embedding_types)
 
-# tagger = SequenceTagger(hidden_size=256, embeddings=embeddings, tag_dictionary=label_dict, tag_type=label_type)
+# tagger = SequenceTagger(hidden_size=1024, embeddings=embeddings, tag_dictionary=label_dict, tag_type=label_type)
 # trainer = ModelTrainer(tagger,corpus)
-# trainer.train('resources/taggers/iam_ner_flair_gloves_18_custom_perfect', learning_rate=0.01, mini_batch_size=64, max_epochs=150, embeddings_storage_mode='none')
+# trainer.train(f'resources/taggers/iam_ner_flair_gloves_forward_{dataset}', learning_rate=0.01, mini_batch_size=16, max_epochs=150, embeddings_storage_mode='cpu', optimizer= optim)
+
+#from transformers import LlamaTokenizer
